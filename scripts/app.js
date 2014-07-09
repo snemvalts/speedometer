c4d0014bace3514927f6d9088220276f83b5eed8
Sander Nemvalts <snemvalts@gmail.com>
2014-07-09 23:13:58 +0300

    now it logs

--- scripts/app.js
+++ scripts/app.js
@@ -5,12 +5,14 @@ app.convertSpeed = function(speed){
 }
 app.init = function(){
 	if(navigator.geolocation){
-		navigator.geolocation.getCurrentPosition(app.getSpeed)
+		navigator.geolocation.getCurrentPosition(app.getData,app.failData,{enableHighAccuracy:true,maximumAge:0})
 	}
 }
-app.getSpeed = function(position){
+app.getData = function(position){
 	console.log("Latitude:",position.coords.latitude)
-	console.log("Longtitude:",position.coords.longtitude)
+	console.log("Longtitude:",position.coords.longitude)
 	console.log("Altitude:",position.coords.altitude)
 	console.log("Speed:",position.coords.speed)
+	document.querySelector("#locationdata").innerHTML = "Latitude: " + position.coords.latitude+ ", Longtitude: " + position.coords.longitude +", Altitude:"+position.coords.altitude+", Speed:"+position.coords.speed
 }
+app.init()
